84098c05c0f424b4a58d75ef2ad143fe3f01bd0b
Saeed Hajebi <hajebis@scss.tcd.ie>
2014-09-20 12:56:57 +0100

    3rd commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Programming Assignment 2
 
-## Write a short comment describing this function
+## Caching data  
 
+# Create matrix
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Returns the cached reveresed matrix if it is avalibale, 
+#  otherwise calcluate the matrix revese and return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    mat = x$get()
+    # It's possible to use ginv() function in the MASS library to be able to deal with general matrices.
+    #library(MASS)
+    #m = ginv(mat)
+    m = solve(mat)
+    
+    x$setInverse(m)
+    m
+    
 }
+
+
